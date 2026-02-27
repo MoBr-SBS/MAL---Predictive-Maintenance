bca4e6a4e17779b752dd35e9ab2115375c3648ba
MB-SBS <moritz.brendel@sbs-herzogenaurach.de>
2026-02-27 03:13:17 -0800

    Created first ai-training code with a neural network. In the future we should switch to boosting-procedure.

---
+++ correlation.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+path = "predictive_maintenance.csv"
+data = pd.read_csv(path, delimiter=',')
+
+# Ausgabe der Korrelationen
+correlations = data[data.columns].corr(numeric_only=True)
+#print(correlations)
+print('All correlations')
+print('-' * 30)
+correlations_abs_sum = correlations[correlations.columns].abs().sum()
+print(correlations_abs_sum)
+print('Weakest correlations')
+print('-' * 30)
+print(correlations_abs_sum.nsmallest(3))
